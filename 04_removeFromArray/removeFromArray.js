08e9d7b70678dfae006fe451c3d6ff9616b6d2ba
Brooks Anderson <bilbrobaggins@gmail.com>
2024-05-12 22:27:30 -0500

    Remove array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...values) {
+    remArr = [];
+    arr.forEach((item) => {
+        if (!values.includes(item)) {
+            remArr.push(item);
+        }
+    });
+    return remArr;
 };
 
 // Do not edit below this line
