55b6e98b7528644a252b3cdafabab0b13836a3b0
Brooks Anderson <bilbrobaggins@gmail.com>
2024-05-12 22:40:12 -0500

    sumall

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || !Number.isInteger(num1)) return "ERROR";
+    if (num2 < 0 || !Number.isInteger(num2)) return "ERROR";
 
+    start = (num1 < num2) ? num1 : num2;
+    end = (num1 < num2) ? num2 : num1;
+
+    sum = 0;
+    for (i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
