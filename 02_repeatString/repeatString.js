97443a9193d4bd4d216e896a78f31f92d022ae47
Brooks Anderson <bilbrobaggins@gmail.com>
2024-05-12 21:58:38 -0500

    repeat str

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, number) {
+    if (number < 0) {
+        return "ERROR";
+    }
+    
+    let res = "";
+    for (let x = 0; x < number; x++) {
+        res += str;
+    }
+    return res;
 };
 
 // Do not edit below this line
