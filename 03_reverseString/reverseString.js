46c942d644c1142caf988423ab4f54c5b89e07e9
Brooks Anderson <bilbrobaggins@gmail.com>
2024-05-12 21:58:47 -0500

    rev str

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let array = [];
+    
+    for (i = 0; i < string.length; i++) {
+        array[i] = string[string.length - i - 1];
+    }
 
+    revStr = array.join('');
+    return revStr;
 };
 
 // Do not edit below this line
