fa26bf4051a6652223945297e59b6c4a0083640d
Brooks Anderson <bilbrobaggins@gmail.com>
2024-05-15 20:49:10 -0500

    leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year_in) {
+    let year = Number(year_in)
+    let result;
+    if ((year % 4 === 0) && (year % 100 === 0) && (year % 400 === 0)) {
+        result = true;
+    } else if ((year % 4 === 0) && (year % 100 === 0)) {
+        result = false;
+    } else if (year % 4 === 0) {
+        result = true;
+    } else {
+        result = false;
+    }
 
+    return result;
 };
 
 // Do not edit below this line
